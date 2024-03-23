488dfb1646d3ec3459ee0daa4df15a0991473a87
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-23 02:50:52 +0200

    Add files via upload

---
+++ picon_defult-picon.sh.sh
@@ -0,0 +1,10 @@
+wget -O /media/dreambox-storage/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
+
+wait
+wget -O /media/hdd/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
+
+wait
+wget -O /usr/share/enigma2/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
+
+sleep 2;
+exit 0ww
