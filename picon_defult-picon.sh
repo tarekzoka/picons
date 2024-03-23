53c10b27d86a952db397e71657678f764e5ed343
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-23 02:48:25 +0200

    Update picon_defult-picon.sh

--- picon_defult-picon.sh
+++ picon_defult-picon.sh
@@ -1,7 +1,9 @@
 wget -O /media/dreambox-storage/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
 
 wait
+wget -O /media/hdd/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
 
+wait
 wget -O /usr/share/enigma2/picon/picon_default.png https://raw.githubusercontent.com/tarekzoka/settingsz/main/picon_default.png
 
 sleep 2;
